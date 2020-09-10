63d4ce8d13284b62b3c09dc82d136e200de2e509
DaRealNim <darealnim@gmail.com>
2020-09-10 22:38:03 +0200

    english < french

--- bases.py
+++ bases.py
@@ -2,18 +2,18 @@ import sys
 import math
 
 def main():
-    b1chars = input("Entrez les caractères de la base de départ: ")
+    b1chars = input("Enter characters for the base to convert from: ")
     b1 = list(b1chars)
     if len(b1) > len(set(b1)):
-        print("Erreur: tout les caractères de la base doivent être uniques.")
+        print("Error: all characters must be unique.")
         sys.exit(1)
-    print("(base %d enregistrée)"%len(b1))
-    b2chars = input("Entez les caractères de la base d'arrivée: ")
+    print("(base %d loaded)"%len(b1))
+    b2chars = input("Enter characters for the base to convert to: ")
     b2 = list(b2chars)
     if len(b2) > len(set(b2)):
-        print("Erreur: tout les caractères de la base doivent être uniques.")
+        print("Error: all characters must be unique.")
         sys.exit(1)
-    print("(base %d enregistrée)"%len(b2))
+    print("(base %d loaded)"%len(b2))
     print("")
     while True:
         inp = input(">")
@@ -25,7 +25,7 @@ def main():
         p = 0
         for c in inp[::-1]:
             if not c in b1:
-                print("Error: caractère non reconnu dans la base de départ (%s)"%c)
+                print("Error: unknown character in starting base (%s)"%c)
                 continue
             input_to_b10 += int(b1.index(c)*math.pow(len(b1), p))
             p+=1
